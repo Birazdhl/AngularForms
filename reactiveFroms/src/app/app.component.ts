0bff26c0283bcb1775e1c1b3100de7ce3ec76567
birazdhl <birazdhl@gmail.com>
2020-06-15 22:33:34 +0545

    Final Commit

--- reactiveFroms/src/app/app.component.ts
+++ reactiveFroms/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators, FormGroup, FormArray } from '@angular/forms';
 import { forbiddenNameValidator } from './shared/userName.validator';
 import { PasswordValidator } from './shared/password.validator';
+import { RegistrationService } from './registration.service';
 
 @Component({
   selector: 'app-root',
@@ -29,7 +30,7 @@ export class AppComponent implements OnInit{
     this.alternateEmails.push(this.fb.control(''));
   }
 
-  constructor(private fb: FormBuilder){}
+  constructor(private fb: FormBuilder, private _registrationService: RegistrationService){}
 
   ngOnInit() {
     
@@ -85,6 +86,15 @@ export class AppComponent implements OnInit{
         postalCode : '123456'
       }
    })
+ }
+
+ onSubmit() {
+   console.log(this.registrationForm.value);
+
+   this._registrationService.register(this.registrationForm.value)
+       .subscribe( 
+                  response => console.log('Success', response),
+                    error => console.error('Error!',error));
  }
 
 }
